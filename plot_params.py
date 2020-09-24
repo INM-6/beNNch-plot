23d14b31a99ad6b591464c9333c874627ba8275a
jasperalbers <jasperalbers45@gmail.com>
2020-09-24 10:08:14 +0200

    changed to new color scheme

--- plot_params.py
+++ plot_params.py
@@ -14,38 +14,53 @@ matplotlib_params = {
 }
 
 additional_params = {
-    'figsize_single': [6.1, 6.1/3],
-    'figsize_double': [12.2, 12.2/3]
+    'figsize_single': [6.1*1.5, 6.1],
+    'figsize_double': [12.2, 6.1]
+    # 'figsize_double': [12.2*1.5, 6.1]
 }
 
+# color_params = {
+#     'wall_time_total': '#bebebe', # Black
+#     'real_time_factor': '#bebebe', # Black
+#     'wall_time_sim': '#ff9f9a', # Peach
+#     'wall_time_creation+wall_time_connect': '#64b5cd', # Light Blue
+#     'wall_time_phase_update': '#c44e52', # Brownish red
+#     'wall_time_phase_deliver': '#4c72b0', # Dark Blue
+#     'wall_time_phase_communicate': '#55a868', # Light green
+#     'frac_phase_update': '#c44e52', # Brownish red
+#     'frac_phase_deliver': '#4c72b0', # Dark Blue
+#     'frac_phase_communicate': '#55a868', # Light green
+#     }
+
 color_params = {
-    'wall_time_total': '#bebebe', # Black
-    'real_time_factor': '#bebebe', # Black
-    'wall_time_sim': '#ff9f9a', # Peach
-    'wall_time_creation+wall_time_connect': '#64b5cd', # Light Blue
-    'wall_time_phase_update': '#c44e52', # Brownish red
-    'wall_time_phase_deliver': '#4c72b0', # Dark Blue
-    'wall_time_phase_communicate': '#55a868', # Light green
-    'frac_phase_update': '#c44e52', # Brownish red
-    'frac_phase_deliver': '#4c72b0', # Dark Blue
-    'frac_phase_communicate': '#55a868', # Light green
+    'wall_time_total': '#AA3377', # purple
+    'real_time_factor': '#BBBBBB', # gray
+    'wall_time_sim': '#CCBB44', # yellow
+    'wall_time_creation+wall_time_connect': '#66CCEE', # cyan
+    'wall_time_phase_update': '#EE6677', # red
+    'wall_time_phase_deliver': '#0077BB', # blue
+    'wall_time_phase_communicate': '#228833', # green
+    'frac_phase_update': '#EE6677', # red
+    'frac_phase_deliver': '#0077BB', # blue
+    'frac_phase_communicate': '#228833', # green
     }
 
 label_params = {
     'num_omp_threads': 'OMP threads',
     'num_mpi_tasks': 'MPI processes',
     'num_nodes': 'Nodes',
-    'wall_time_total': 'wall time [s]',
-    'wall_time_preparation': 'wall time preparation [s]',
-    'wall_time_presim': 'presimulation time',
-    'wall_time_creation': 'creation time',
-    'wall_time_connect': 'connection time',
-    'wall_time_sim': 'simulation time',
-    'wall_time_phase_total': 'total phase time',
+    'wall_time_total': 'total',
+    'wall_time_preparation': 'preparation',
+    'wall_time_presim': 'presimulation',
+    'wall_time_creation': 'creation',
+    'wall_time_connect': 'connection',
+    'wall_time_sim': 'simulation',
+    'wall_time_phase_total': 'total phase',
     'wall_time_phase_update': 'update',
     'wall_time_phase_collocate': 'collocate',
     'wall_time_phase_communicate': 'communicate',
     'wall_time_phase_deliver': 'deliver',
+    'wall_time_creation+wall_time_connect': 'network construction',
     'max_memory': 'memory',
     'real_time_factor': 'real time factor',
     'frac_phase_update': 'update',
