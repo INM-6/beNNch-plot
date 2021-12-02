cb185db1d26eb616ca56c36bbf376d3bce889103
jasperalbers <jasperalbers@posteo.de>
2021-12-02 17:09:31 +0100

    adapt examples to work with updated plotting routines

--- examples/multi-area-model/multi-area-model_ram.py
+++ examples/multi-area-model/multi-area-model_ram.py
@@ -4,9 +4,7 @@ import matplotlib.gridspec as gridspec
 
 
 args = {
-    'data_hash': 'a9d9b91e-e3da-4f91-b9c2-2f8c60347c53',
-    'data_path': '../../timing_results',
-    'catalogue_path': '../../catalogue.yaml',
+    'data_file': '45011f6d-c3c2-4f2c-b884-af04e9edc5b9.csv',
     'x_axis': ['num_nodes'],
     'time_scaling': 1e3
 }
@@ -18,10 +16,10 @@ B = bp.BenchPlot(**args)
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 6))
 
 B.plot_main(quantities=['total_memory'], axis=ax,
-            error=True)
+            error=True, fmt='-')
 
-ax.set_xlabel('Number of Nodes')
+ax.set_xlabel('Number of nodes')
 ax.set_ylabel('RAM [B]')
 
 ax.legend()
-plt.savefig('ram_3.0_Fig5_JUSUF.pdf')
+plt.savefig('ram_usage.pdf')
